2a047d92d537750f7b4bbcba8dbb72e9084222d6
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2014-02-14 14:26:34 +0900

    license: review
    and add original COPYING

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(name='cpylmnfct',
       author='Ken-ichirou MATSUZAWA',
       author_email='chamas@h4.dion.ne.jp',
       url='https://github.com/chamaken/cpylmnfct',
-      license='GPLv2+',
+      license='LGPLv2+',
       packages=['cpylmnfct',],
-      classifiers=['License :: OSI Approved :: GNU General Public ' +
-                   'License v2 or later (GPLv2+)',
+      classifiers=['License :: OSI Approved :: GNU Lesser General ' +
+                   'Public License v2 or later (LGPLv2+)',
                    'Programming Language :: Python',
                    'Topic :: Software Development :: Libraries :: ' +
                    'Python Modules',
